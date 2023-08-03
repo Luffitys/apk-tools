2ab1206251ad47d93577f238bbec609a5dc0a6d5
Daniel Banacki <banackidaniel@gmail.com>
2023-08-03 17:37:52 +0200

    Use 7zr to extract 7za, prepare for magisk mmt

---
+++ apk-tools/make_magisk-module.bat
@@ -0,0 +1,3 @@
+@echo off
+call "scripts\dont_run-set-variables.bat"
+
--- apk-tools/scripts/dont_run-download-dependencies.bat
+++ apk-tools/scripts/dont_run-download-dependencies.bat
@@ -23,7 +23,26 @@ for %%f in (%adb_files%) do (
     )
 )
 
-if not exist "%bin_path%\7z.exe" (
-    echo Downloading 7z
-    curl -o "%bin_path%\7z.exe" -sLJO https://www.7-zip.org/a/7zr.exe
+if not exist "%bin_path%\7zr.exe" (
+    echo Downloading 7zr
+    curl -o "%bin_path%\7zr.exe" -sLJO https://www.7-zip.org/a/7zr.exe
+)
+
+if not exist "%bin_path%\7z" (
+    echo Downloading 7z standalone
+    curl -o "%bin_path%\7z.7z" -sLJO https://www.7-zip.org/a/7z2301-extra.7z
+    echo Extracting 7z standalone
+    "%bin_path%\7zr.exe" x "%bin_path%\7z.7z" -o"%bin_path%\7z"
+    del "%bin_path%\7z.7z"
+    echo.
+)
+
+if not exist "%bin_path%\mmt" (
+    echo Downloading Magisk MMT template
+    curl -o "%bin_path%\mmt.zip" -sLJO https://github.com/Zackptg5/MMT-Extended/archive/refs/heads/master.zip
+    echo.
+    echo Extracting Magisk MMT template
+    "%bin_path%\7z\7za.exe" x "%bin_path%\mmt.zip" -o"%bin_path%\mmt"
+    del "%bin_path%\mmt.zip"
+    echo.
 )
